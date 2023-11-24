a8b74251feeb4fcff5b218b95b6a8bd321c7a030
Inmaculada Parra <amyparrafernandez@hotmail.com>
2023-11-24 12:08:11 +0000

    casteos

--- models/staging/sql_server_dbo/stg_orders.sql
+++ models/staging/sql_server_dbo/stg_orders.sql
@@ -17,28 +17,28 @@ source as (
 stg_orders as (
 
 select
-        decode(promo_id,'',{{ dbt_utils.generate_surrogate_key(['9999']) }},{{ dbt_utils.generate_surrogate_key(['upper(promo_id)']) }}) as id_promo,
-        decode(promo_id,'','WITHOUT-PROMO',upper(promo_id)) as  promo_desc,
-        order_id,
-        shipping_service,
-        shipping_cost,
-        address_id,
-        created_at,
-        estimated_delivery_at,
-        order_cost,
-        user_id,
-        order_total,
-        delivered_at,
-        tracking_id,
-        status,
-        _fivetran_deleted,
-        _fivetran_synced
+        cast(decode(promo_id,'',{{ dbt_utils.generate_surrogate_key(['9999']) }},{{ dbt_utils.generate_surrogate_key(['upper(promo_id)']) }}) as varchar) as id_promo,
+        cast(decode(promo_id,'','WITHOUT-PROMO',upper(promo_id)) as varchar) as  promo_description,
+        cast(order_id as varchar) as id_order,
+        cast(shipping_service as varchar) as shipping_service,
+        cast(shipping_cost as float) as shipping_cost_USD,
+        cast({{ dbt_utils.generate_surrogate_key(['address_id']) }} as varchar) as id_address,
+        cast(created_at as date) as created_at,
+        cast(estimated_delivery_at as date) as estimated_delivery_at,
+        cast(order_cost as float) as order_cost_USD,
+        cast(user_id as varchar) as id_user,
+        cast(order_total as float) as order_total_USD,
+        cast(delivered_at as date) as delivered_at,
+        cast(tracking_id as varchar) as id_tracking,
+        cast(status as varchar) as status,
+        _fivetran_deleted as date,
+        _fivetran_synced as date
 
     from source
 
 )
 
-select * from stg_orders;
+select * from stg_orders
 
 
 
