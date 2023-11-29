591f655b1fcc7e5b43b7761f302b02d477c97193
Inmaculada Parra <amyparrafernandez@hotmail.com>
2023-11-29 13:22:50 +0000

    update

--- analyses/Ejercicios/Ejercicio4.sql
+++ analyses/Ejercicios/Ejercicio4.sql
@@ -1,34 +1,34 @@
 
 
-{{
-    config(
-        materialized = 'ephemeral'
-    )
+-- {{
+--     config(
+--         materialized = 'ephemeral'
+--     )
 
-}}
+-- }}
 
-WITH stg_events AS (
-    SELECT 
-    year(created_at)*10000+month(created_at)*100+day(created_at) as id_date_created
-    created_at as time_created
+-- WITH stg_events AS (
+--     SELECT 
+--     year(created_at)*10000+month(created_at)*100+day(created_at) as id_date_created
+--     created_at as time_created
 
-    from src_sql_events
-    FROM {{ ref('stg_events') }}
-),
+--     from src_sql_events
+--     FROM {{ ref('stg_events') }}
+-- ),
 
 
-single_session AS (
-    SELECT
+-- single_session AS (
+--     SELECT
     
-    year(created_at)*1000000+month(created_date)*10000+day(created_at)*100+HOUR(creation_time) as hora,
-    count(distinct session_id) as num_sesiones,
-    FROM stg_events
-    group by 1
-    FROM stg_events
-    )
+--     year(created_at)*1000000+month(created_date)*10000+day(created_at)*100+HOUR(creation_time) as hora,
+--     count(distinct session_id) as num_sesiones,
+--     FROM stg_events
+--     group by 1
+--     FROM stg_events
+--     )
 
-SELECT avg(num_sesiones) as Media_Sesiones 
-from single_session
+-- SELECT avg(num_sesiones) as Media_Sesiones 
+-- from single_session
 
 
-select * from stg_events
+-- select * from stg_events
