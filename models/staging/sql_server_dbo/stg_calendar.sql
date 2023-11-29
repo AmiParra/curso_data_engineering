a518e5c52305588c9b443fcc67a3b9c53f94821b
Inmaculada Parra <amyparrafernandez@hotmail.com>
2023-11-29 12:46:42 +0000

    update

--- models/staging/sql_server_dbo/stg_calendar.sql
+++ models/staging/sql_server_dbo/stg_calendar.sql
@@ -1,39 +1,80 @@
 
 
-{{ 
-    config(
-        materialized='table', 
-        sort='date_day',
-        dist='date_day',
-        pre_hook="alter session set timezone = 'Europe/Madrid'; alter session set week_start = 7;" 
-        ) }}
+-- {{ 
+--     config(
+--         materialized='table', 
+--         sort='date_day',
+--         dist='date_day',
+--         pre_hook="alter session set timezone = 'Europe/Madrid'; alter session set week_start = 7;" 
+--         ) }}
 
 
 
-WITH date_spine AS (
+-- WITH date_spine AS (
 
-  {{ dbt_utils.date_spine(
-      start_date="to_date('01/01/2021', 'mm/dd/yyyy')",
-      datepart="day",
-      end_date="dateadd(year, 10, current_date()+1)"
-     )
-  }}
+--   {{ dbt_utils.date_spine(
+--       start_date="to_date('01/01/2021', 'mm/dd/yyyy')",
+--       datepart="day",
+--       end_date="dateadd(year, 10, current_date()+1)"
+--      )
+--   }}
 
-), calculated as (
+-- ) 
 
-    SELECT
-      date_day,
-      DAYNAME(date_day) as day_name,
-      DATE_PART('month', date_day) as month,
-      DATE_PART('year', date_day) as year
-      from date_spine )
+--   SELECT
+--       year(date_day)*10000+month(date_day)*100+day(date_day) as id_date,
+--       date_day
+    --   DAYNAME(date_day) as day_name,
+    --   month(date_day) as mes,
+    --   monthname(date_day) as desc_mes,
+    --   year(date_day)*100+month(date_day) as id_anio_mes,
+    --   DATE_PART('month', date_day) as month,
+    --   DATE_PART('year', date_day) as year,
+    --   year(date_day)||weekiso(date_day)||dayofweek(date_day) as anio_semana_dia,
+    --   weekiso(date_day) as semana
+-- from date_spine 
+-- order by
+--     date_day desc
 
 
-select * from calculated
-order by date_day desc
+-- --select * from calculated
+-- order by date_day desc
 
 
 
 --from calculated
 --order by 
 --    date_day desc
+
+
+-- {{ 
+--     config(
+--         materialized='table', 
+--         sort='date_day',
+--         dist='date_day',
+--         pre_hook="alter session set timezone = 'Europe/Madrid'; alter session set week_start = 7;" 
+--         ) }}
+
+-- with date as (
+--     {{ dbt_utils.date_spine(
+--         datepart="day",
+--         start_date="cast('2000-01-01' as date)",
+--         end_date="cast(current_date()+1 as date)"
+--     )
+--     }}  
+-- )
+
+
+-- select
+--       date_day as fecha_forecast
+--     , year(date_day)*10000+month(date_day)*100+day(date_day) as id_date
+--     , year(date_day) as anio
+--     , month(date_day) as mes
+--     ,monthname(date_day) as desc_mes
+--     , year(date_day)*100+month(date_day) as id_anio_mes
+--     , date_day-1 as dia_previo
+--     , year(date_day)||weekiso(date_day)||dayofweek(date_day) as anio_semana_dia
+--     , weekiso(date_day) as semana
+-- from date
+-- order by
+--     date_day desc
--- models/staging/sql_server_dbo/stg_events.sql
+++ models/staging/sql_server_dbo/stg_events.sql
@@ -1,36 +1,48 @@
-{{
-  config(
-    materialized='table'
-  )
-}}
+-- {{
+--   config(
+--     materialized='table'
+--   )
+-- }}
 
 
 
 
-with 
+-- with 
 
-source as (
+-- source as (
 
-    select * from {{ source('sql_server_dbo', 'events') }}
+--     select * from {{ source('sql_server_dbo', 'events') }}
 
-),
+-- ),
 
-stg_events as (
+-- -- Como práctica, en todos los stg_ del proyecto se hashearan todos los id_ procedentes del 
+-- -- operacional por dos razones:
+-- --      - Para evitar que los id_ del operacional sean los mismos en el informacional (esto 
+-- --        sería una buena practica generalizada para evitar problemas en caso de que cambie
+-- --        la forma de las pk en el operacional)
+-- --      - Asegurar que todos los id sean un hash.
+-- -- Para todo ello se usará la función generate_surrogate_key del paquete dbt_utils
 
-    select
-        event_id,
-        page_url,
-        event_type,
-        user_id,
-        product_id,
-        session_id,
-        created_at,
-        order_id,
-        _fivetran_deleted,
-        _fivetran_synced
+-- REVISAR LA LÓGICA DE LA TABLA EN CUANTO A ORDER ID Y QUE NO HAYA HABIDO CHECK OUT Y DEMAS
 
-    from source
+-- stg_events as (
 
-)
+--     select
+--         {{ dbt_utils.generate_surrogate_key(['event_id']) }} :: varchar as id_event,
+--         event_id,
+--         page_url :: varchar as page_url,
+--         event_type :: varchar as page_url,
+--         {{ dbt_utils.generate_surrogate_key(['user_id']) }} :: varchar as id_user,
+--         user_id,
+--         product_id,
+--         session_id,
+--         created_at,
+--         order_id,
+--         _fivetran_deleted,
+--         _fivetran_synced
 
-select * from stg_events
+--     from source
+
+-- )
+
+-- select * from stg_events
