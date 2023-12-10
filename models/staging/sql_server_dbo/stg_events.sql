066f62c873196bc367a388e953287f39b9ef15f5
Inmaculada Parra <amyparrafernandez@hotmail.com>
2023-12-10 16:31:08 +0000

    updated

---
+++ models/staging/sql_server_dbo/stg_events.sql
@@ -0,0 +1,36 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+
+
+
+with 
+
+source as (
+
+    select * from {{ source('sql_server_dbo', 'events') }}
+
+),
+
+stg_events as (
+
+    select
+        event_id,
+        page_url,
+        event_type,
+        user_id,
+        product_id,
+        session_id,
+        created_at,
+        order_id,
+        _fivetran_deleted,
+        _fivetran_synced
+
+    from source
+
+)
+
+select * from stg_events
